d17ae723121ae0e9fd7c24728c657c086a15011f
jacqueline-homan <jacquelinehoman7@gmail.com>
2015-10-03 02:00:29 -0400

    Demonstrating loops and conditions

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpConsoleApp
+{
+	class Program
+	{
+		static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+
+			#region Loops
+			Console.WriteLine("Here we demonstrate a for loop");
+			for (int counter = 0; counter < 5; counter++)
+			{
+				Console.WriteLine(counter);
+			}
+			Console.WriteLine("~~~~Oh boy! You can count!~~~~");
+			Console.WriteLine("Next, we demonstrate a foreach loop");
+			int[] arrInts =  new int[] {3,5,7,9};
+			foreach (int item in arrInts)
+			{
+				Console.WriteLine(item);
+				Console.WriteLine("");
+				Console.WriteLine("Notice that the array's output looks like a list of numbers");
+			}
+			Console.WriteLine(" ");
+			Console.WriteLine("50 points if you can guess why the above sentence was repeated 4 times");
+			Console.WriteLine("If you guessed that it was part of the foreach loop body");
+			Console.WriteLine("that is executed for each element in the array, you win 50 points!");
+			Console.WriteLine("");
+			Console.WriteLine("Now we'll do a while loop");
+
+			int sentinel = 0;
+			while (sentinel < 5)
+			{
+				Console.WriteLine(sentinel);
+				sentinel++;
+			}
+
+			Console.WriteLine("Next we demonstrate the do-while loop");
+			Console.WriteLine("It executes the code inside the do-block if conditions are met");
+			Console.WriteLine("by evaluating the condition at the end of the loop.");
+			Console.WriteLine("As long as the condition at the end is met,");
+			Console.WriteLine("the code inside the do-while loop is executed.");
+			int sentinel2 = 5;
+			do
+			{
+				Console.WriteLine(sentinel2);
+				sentinel2++;
+			} while (sentinel2 < 10);
+
+			#endregion
+
+			#region Decision Structures
+			//if statement
+			bool result = true;
+			if (result)
+			{
+				Console.WriteLine("Result was true");
+			}
+			else
+			{
+				Console.WriteLine("Result was false");
+			}
+
+			#endregion
+		}
+	
+	
+
+		//for loop
+		//for (int counter = 0; counter < 10; counter++)
+		//{
+		//    Console.WriteLine(counter);
+		//}
+
+
+
+	}
+
+
+}
